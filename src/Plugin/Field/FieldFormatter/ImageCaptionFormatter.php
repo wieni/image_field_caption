d3f2ecad86f60c9851d797aae4b41f11b60f79ea
Echofive <m@matthieuroy.be>
2016-10-25 13:18:45 +0200

    Fix - Notice: Undefined variable: langcode in line 168 of core/modules/image/src/Plugin/Field/FieldFormatter/ImageFormatter.php. Fix - Warning: Missing argument 2 for Drupal\image\Plugin\Field\FieldFormatter\ImageFormatter::viewElements(), called in /modules/contrib/image_field_caption/src/Plugin/Field/FieldFormatter/ImageCaptionFormatter.php on line 30 and defined in Drupal\image\Plugin\Field\FieldFormatter\ImageFormatter->viewElements() (line 166 of core/modules/image/src/Plugin/Field/FieldFormatter/ImageFormatter.php).

--- src/Plugin/Field/FieldFormatter/ImageCaptionFormatter.php
+++ src/Plugin/Field/FieldFormatter/ImageCaptionFormatter.php
@@ -27,7 +27,7 @@ class ImageCaptionFormatter extends ImageFormatter {
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    $elements = parent::viewElements($items);
+    $elements = parent::viewElements($items, $langcode);
     foreach ($elements as $delta => $element) {
       // Set a new theme callback function for the image caption formatter.
       $elements[$delta]['#theme'] = 'image_caption_formatter';
